1677fb6e613302d70d1f92d8e6cd083b80bed448
kisah <zazza2008@yandex.ru>
2021-06-01 19:42:13 +0300

    Building the test framework

--- src/tests/testlogger.cpp
+++ src/tests/testlogger.cpp
@@ -1,23 +1,136 @@
 #include "testlogger.h"
+#include <iostream>
 
 using namespace Tests;
 
 void TestLogger::open(std::string binpath, pid_t pid, std::string path, int mode, int fd) {
-
+    if(m_active)
+        m_records.emplace_back(OpRecord(RECORD_OPEN, pid, path, mode, 0, 0));
 }
 
 void TestLogger::open_failed(std::string binpath, pid_t pid, std::string path, int mode, int error) {
-
+    if(m_active)
+        m_records.emplace_back(OpRecord(RECORD_OPEN_ERROR, pid, path, mode, 0, error));
 }
 
 void TestLogger::read(std::string binpath, pid_t pid, std::string path, int fd, size_t len) {
-
+    if(m_active)
+        m_records.emplace_back(OpRecord(RECORD_READ, pid, path, 0, len, 0));
 }
 
 void TestLogger::write(std::string binpath, pid_t pid, std::string path, int fd, size_t len) {
-
+    if(m_active)
+        m_records.emplace_back(OpRecord(RECORD_WRITE, pid, path, 0, len, 0));
 }
 
 void TestLogger::close(std::string binpath, pid_t pid, std::string path, int fd) {
+    if(fd == -10)
+        m_active = !m_active;
+    else if(m_active)
+        m_records.emplace_back(OpRecord(RECORD_CLOSE, pid, path, 0, 0, 0));
+}
 
+void TestLogger::expect_open(pid_t pid, std::string path, int mode) {
+    if(!m_records.size())
+        throw "Record list is empty";
+    
+    auto record = m_records.front();
+    m_records.pop_front();
+
+    if(record.type != RECORD_OPEN)
+        throw "Record type doesn't match";
+
+    if(record.pid != pid && pid != -1)
+        throw "Process id doesn't match";
+
+    if(record.path != path)
+        throw "Path doesn't match";
+
+    if(record.mode != mode)
+        throw "Open mode doesn't match";
+}
+
+void TestLogger::expect_open_failed(pid_t pid, std::string path, int mode, int error) {
+    if(!m_records.size())
+        throw "Record list is empty";
+    
+    auto record = m_records.front();
+    m_records.pop_front();
+
+    if(record.type != RECORD_OPEN)
+        throw "Record type doesn't match";
+
+    if(record.pid != pid && pid != -1)
+        throw "Process id doesn't match";
+
+    if(record.path != path)
+        throw "Path doesn't match";
+
+    if(record.mode != mode)
+        throw "Open mode doesn't match";
+
+    if(record.error != error)
+        throw "Unexpected error condition";
+}
+
+void TestLogger::expect_read(pid_t pid, std::string path, size_t len) {
+    if(!m_records.size())
+        throw "Record list is empty";
+    
+    auto record = m_records.front();
+    m_records.pop_front();
+
+    if(record.type != RECORD_READ)
+        throw "Record type doesn't match";
+
+    if(record.pid != pid && pid != -1)
+        throw "Process id doesn't match";
+
+    if(record.path != path)
+        throw "Path doesn't match";
+
+    if(record.len != len)
+        throw "Read length doesn't match";
+}
+
+void TestLogger::expect_write(pid_t pid, std::string path, size_t len) {
+    if(!m_records.size())
+        throw "Record list is empty";
+    
+    auto record = m_records.front();
+    m_records.pop_front();
+
+    if(record.type != RECORD_WRITE)
+        throw "Record type doesn't match";
+
+    if(record.pid != pid && pid != -1)
+        throw "Process id doesn't match";
+
+    if(record.path != path)
+        throw "Path doesn't match";
+
+    if(record.len != len)
+        throw "Write length doesn't match";
+}
+
+void TestLogger::expect_close(pid_t pid, std::string path) {
+    if(!m_records.size())
+        throw "Record list is empty";
+    
+    auto record = m_records.front();
+    m_records.pop_front();
+
+    if(record.type != RECORD_CLOSE)
+        throw "Record type doesn't match";
+
+    if(record.pid != pid && pid != -1)
+        throw "Process id doesn't match";
+
+    if(record.path != path)
+        throw "Path doesn't match";
+}
+
+void TestLogger::expect_empty_list() {
+    if(m_records.size())
+        throw "Record list is not empty";
 }
--- src/tests/testlogger.h
+++ src/tests/testlogger.h
@@ -1,8 +1,28 @@
 #pragma once
 #include <string>
+#include <deque>
 #include "../main/logger.h"
 
 namespace Tests {
+
+enum RecordType {
+    RECORD_OPEN,
+    RECORD_OPEN_ERROR,
+    RECORD_READ,
+    RECORD_WRITE,
+    RECORD_CLOSE
+};
+
+struct OpRecord {
+    OpRecord(RecordType _type, pid_t _pid, std::string _path, int _mode, size_t _len, int _error) : type(_type), pid(_pid), path(_path), mode(_mode), len(_len), error(_error) {}
+
+    RecordType type;
+    pid_t pid;
+    std::string path;
+    int mode;
+    size_t len;
+    int error;
+};
 
 class TestLogger : public Logger {
 public:
@@ -11,6 +31,16 @@ public:
     virtual void read(std::string binpath, pid_t pid, std::string path, int fd, size_t len);
     virtual void write(std::string binpath, pid_t pid, std::string path, int fd, size_t len);
     virtual void close(std::string binpath, pid_t pid, std::string path, int fd);
+
+    void expect_open(pid_t pid, std::string path, int mode);
+    void expect_open_failed(pid_t pid, std::string path, int mode, int error);
+    void expect_read(pid_t pid, std::string path, size_t len);
+    void expect_write(pid_t pid, std::string path, size_t len);
+    void expect_close(pid_t pid, std::string path);
+    void expect_empty_list();
+private:
+    bool m_active = false;
+    std::deque<OpRecord> m_records;
 };
 
 }
