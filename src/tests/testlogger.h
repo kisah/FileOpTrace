b2f09b16d6fd16978e018681aac24da9a70ac774
kisah <zazza2008@yandex.ru>
2021-06-01 15:30:17 +0300

    Added TestLogger stub

---
+++ src/tests/testlogger.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#include "../main/logger.h"
+
+class TestLogger : public Logger {
+public:
+    virtual void open(std::string binpath, pid_t pid, std::string path, int mode, int fd);
+    virtual void open_failed(std::string binpath, pid_t pid, std::string path, int mode, int error);
+    virtual void read(std::string binpath, pid_t pid, std::string path, int fd, size_t len);
+    virtual void write(std::string binpath, pid_t pid, std::string path, int fd, size_t len);
+    virtual void close(std::string binpath, pid_t pid, std::string path, int fd);
+};
