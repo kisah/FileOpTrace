08725b813b56a12d46729afd36fb1dfe83ba68cf
kisah <zazza2008@yandex.ru>
2021-06-02 16:24:28 +0300

    Added the first five tests

--- src/testbuddy/testbuddy.cpp
+++ src/testbuddy/testbuddy.cpp
@@ -1,12 +1,54 @@
 #include <iostream>
+#include <string>
 #include <fcntl.h>
 #include <unistd.h>
+#include <dirent.h>
 
-int main(int argc, char** argv) {
-    close(-10); //mark the beggining of tests
-
+void test_simple() {
     int fd = open("/dev/null", O_WRONLY);
     close(fd);
+}
+
+void test_relative() {
+    int fd = open("../../hello", O_RDWR);
+    close(fd);
+}
+
+void test_openatcwd() {
+    int fd = openat(AT_FDCWD, "hello", O_RDWR);
+    close(fd);
+}
+
+void test_openatfd() {
+    DIR* dir = opendir("..");
+    int fd = openat(dirfd(dir), "hello", O_RDWR);
+    close(fd);
+    closedir(dir);
+}
+
+void test_nonexistent() {
+    open("doesntexist", O_RDONLY);
+}
+
+int main(int argc, char** argv) {
+    if(argc < 2) {
+        std::cerr << "Usage: testbuddy <test name>" << std::endl;
+        return 1;
+    }
+    
+    close(-10); //mark the beggining of tests
+
+    std::string test(argv[1]);
+    if(test == "simple")
+        test_simple();
+    else if(test == "relative")
+        test_relative();
+    else if(test == "openatcwd")
+        test_openatcwd();
+    else if(test == "openatfd")
+        test_openatfd();
+    else if(test == "nonexistent")
+        test_nonexistent();
 
     close(-10); //mark the end of tests
     return 0;
--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -1,4 +1,6 @@
 #include <fcntl.h>
+#include <limits.h>
+#include <unistd.h>
 #include "doctest.h"
 #include "../main/ptrace.h"
 #include "../main/syscall.h"
@@ -19,13 +21,97 @@ bool run_and_wait(Logger* logger, std::string path, std::vector<std::string> arg
     return true;
 }
 
-TEST_CASE("Test of the test framework") {
+TEST_CASE("Simple open and close") {
     TestLogger logger;
 
-    REQUIRE(run_and_wait(&logger, "../testbuddy/testbuddy", { "testbuddy" }));
+    REQUIRE(run_and_wait(&logger, "../testbuddy/testbuddy", { "testbuddy", "simple" }));
 
     REQUIRE_NOTHROW(logger.expect_open(-1, "/dev/null", O_WRONLY));
     REQUIRE_NOTHROW(logger.expect_close(-1, "/dev/null"));
+
+    REQUIRE_NOTHROW(logger.expect_empty_list());
+}
+
+TEST_CASE("Relative path") {
+    TestLogger logger;
+    std::array<char, PATH_MAX> buf;
+    std::string full_path;
+
+    REQUIRE(!close(open("../../hello", O_WRONLY | O_CREAT, 0644)));
+    
+    buf.fill(0);
+    realpath("../../hello", buf.data());
+    full_path = std::string(buf.data());
+    REQUIRE(full_path.length() > 0);
+
+    REQUIRE(run_and_wait(&logger, "../testbuddy/testbuddy", { "testbuddy", "relative" }));
+
+    unlink("../../hello");
+
+    REQUIRE_NOTHROW(logger.expect_open(-1, full_path, O_RDWR));
+    REQUIRE_NOTHROW(logger.expect_close(-1, full_path));
+
+    REQUIRE_NOTHROW(logger.expect_empty_list());
+}
+
+TEST_CASE("Openat cwd") {
+    TestLogger logger;
+    std::array<char, PATH_MAX> buf;
+    std::string full_path;
+
+    REQUIRE(!close(open("hello", O_WRONLY | O_CREAT, 0644)));
+    
+    buf.fill(0);
+    realpath("hello", buf.data());
+    full_path = std::string(buf.data());
+    REQUIRE(full_path.length() > 0);
+
+    REQUIRE(run_and_wait(&logger, "../testbuddy/testbuddy", { "testbuddy", "openatcwd" }));
+
+    unlink("hello");
+
+    REQUIRE_NOTHROW(logger.expect_open(-1, full_path, O_RDWR));
+    REQUIRE_NOTHROW(logger.expect_close(-1, full_path));
+
+    REQUIRE_NOTHROW(logger.expect_empty_list());
+}
+
+TEST_CASE("Openat with a directry fd") {
+    TestLogger logger;
+    std::array<char, PATH_MAX> buf;
+    std::string file_path;
+    std::string dir_path;
+
+    REQUIRE(!close(open("../hello", O_WRONLY | O_CREAT, 0644)));
+    
+    buf.fill(0);
+    realpath("../hello", buf.data());
+    file_path = std::string(buf.data());
+    REQUIRE(file_path.length() > 0);
+
+    buf.fill(0);
+    realpath("..", buf.data());
+    dir_path = std::string(buf.data());
+    REQUIRE(dir_path.length() > 0);
+
+    REQUIRE(run_and_wait(&logger, "../testbuddy/testbuddy", { "testbuddy", "openatfd" }));
+
+    unlink("../hello");
+
+    REQUIRE_NOTHROW(logger.expect_open(-1, dir_path, O_RDONLY));
+    REQUIRE_NOTHROW(logger.expect_open(-1, file_path, O_RDWR));
+    REQUIRE_NOTHROW(logger.expect_close(-1, file_path));
+    REQUIRE_NOTHROW(logger.expect_close(-1, dir_path));
+
+    REQUIRE_NOTHROW(logger.expect_empty_list());
+}
+
+TEST_CASE("Non-existent file") {
+    TestLogger logger;
+
+    REQUIRE(run_and_wait(&logger, "../testbuddy/testbuddy", { "testbuddy", "nonexistent" }));
+
+    REQUIRE_NOTHROW(logger.expect_open_failed(-1, "doesntexist", O_RDONLY, ENOENT));
 
     REQUIRE_NOTHROW(logger.expect_empty_list());
 }
