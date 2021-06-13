419447b69bbad657217db664e3260b533bb49dd0
kisah <zazza2008@yandex.ru>
2021-06-13 15:25:00 +0300

    Documented some tests related stuff

--- src/tests/testhandler.h
+++ src/tests/testhandler.h
@@ -4,15 +4,38 @@
 
 namespace Tests {
 
+/**
+ * \brief A mock trace event handler for testing PTrace::Tracee::read_string
+ */
 class TestHandler {
 public:
+/**
+ * \brief Constructor
+ * \param tracer A PTrace::TraceApi instance to register the created handler in
+ */
 TestHandler(PTrace::TraceApi& tracer);
+
+/**
+ * \brief Registers a test callback for the handler
+ * \param test The callback
+ * 
+ * The handler passes a reference to a Tracee instance and the value of the RAX register to the callback
+ */
 void register_test(std::function<void(PTrace::Tracee&, unsigned long long)> test) { m_test = test; }
 
 protected:
+/**
+ * \brief A handler callback, that gets called when a trace event arrives
+ * \param event The event type (either TRACE_CHILD_CREATED or TRACE_SIGNAL)
+ * \param tracee A reference to a PTrace::Tracee instance, representing a particular process
+ * \param status A waitpid status
+ * 
+ * This handler will invoke the test callback, when the tracee process hits a breakpoint
+ */
 void trace_handler(PTrace::TraceEvent event, PTrace::Tracee& tracee, int status);
 
 private:
+/// The test callback
 std::function<void(PTrace::Tracee&, unsigned long long)> m_test;
 };
 
--- src/tests/testlogger.h
+++ src/tests/testlogger.h
@@ -5,6 +5,9 @@
 
 namespace Tests {
 
+/**
+ * \brief An enum of all possible operation types
+ */
 enum RecordType {
     RECORD_OPEN,
     RECORD_OPEN_ERROR,
@@ -16,18 +19,49 @@ enum RecordType {
     RECORD_MKDIR
 };
 
+/**
+ * \brief A struct that represents an operation
+ * 
+ * Instances of this struct get put into the queue of operations
+ */
 struct OpRecord {
+    /**
+     * \brief Constructor
+     */
     OpRecord(RecordType _type, pid_t _pid, std::string _path, std::string _path2, int _mode, size_t _len, int _error) : type(_type), pid(_pid), path(_path), path2(_path2), mode(_mode), len(_len), error(_error) {}
 
+    /// Operation type
     RecordType type;
+
+    /// Process id
     pid_t pid;
+
+    /// File path
     std::string path;
+
+    /// Second file path (if necessary)
     std::string path2;
+
+    /// Open mode (if necessary)
     int mode;
+
+    /// Operation length (if necessary)
     size_t len;
+
+    /// Error code
     int error;
 };
 
+/**
+ * \brief A mock Logger for testing most of the functionality
+ * 
+ * It logs and saves the sequence of operations that the tracee has performed into a queue
+ * and then this sequence can be verified using a set of expect_* functions,
+ * those functions throw an exception when they encounter an unexpected event or
+ * an empty event queue
+ * 
+ * The start and the end of a test is marked via a a close(-10) call
+ */
 class TestLogger : public FileOpTrace::Logger {
 public:
     virtual void open(std::string binpath, pid_t pid, std::string path, int mode, int fd);
@@ -39,17 +73,86 @@ public:
     virtual void remove(std::string binpath, pid_t pid, std::string path);
     virtual void mkdir(std::string binpath, pid_t pid, std::string path);
 
+    /**
+     * \brief Verifies that the first operation in the queue is an open operation with the specified parameters
+     * \param pid Process id (or -1 for any process)
+     * \param path File path
+     * \param mode Open mode (r/w or both)
+     * \throw std::string Description of the operation type/parameters mismatch
+     */
     void expect_open(pid_t pid, std::string path, int mode);
+
+    /**
+     * \brief Verifies that the first operation in the queue is an failed open operation with the specified parameters
+     * \param pid Process id (or -1 for any process)
+     * \param path File path
+     * \param mode Open mode (r/w or both)
+     * \param error Error code
+     * \throw std::string Description of the operation type/parameters mismatch
+     */
     void expect_open_failed(pid_t pid, std::string path, int mode, int error);
+
+    /**
+     * \brief Verifies that the first operation in the queue is a read operation with the specified parameters
+     * \param pid Process id (or -1 for any process)
+     * \param path File path
+     * \param len The number of read bytes
+     * \throw std::string Description of the operation type/parameters mismatch
+     */
     void expect_read(pid_t pid, std::string path, size_t len);
+
+    /**
+     * \brief Verifies that the first operation in the queue is a write operation with the specified parameters
+     * \param pid Process id (or -1 for any process)
+     * \param path File path
+     * \param len The number of read bytes
+     * \throw std::string Description of the operation type/parameters mismatch
+     */
     void expect_write(pid_t pid, std::string path, size_t len);
+
+    /**
+     * \brief Verifies that the first operation in the queue is a close operation with the specified parameters
+     * \param pid Process id (or -1 for any process)
+     * \param path File path
+     * \throw std::string Description of the operation type/parameters mismatch
+     */
     void expect_close(pid_t pid, std::string path);
+
+    /**
+     * \brief Verifies that the first operation in the queue is a rename operation with the specified parameters
+     * \param pid Process id (or -1 for any process)
+     * \param old_path Old file path
+     * \param new_path New file path
+     * \throw std::string Description of the operation type/parameters mismatch
+     */
     void expect_rename(pid_t pid, std::string old_path, std::string new_path);
+
+    /**
+     * \brief Verifies that the first operation in the queue is a remove operation with the specified parameters
+     * \param pid Process id (or -1 for any process)
+     * \param path File path
+     * \throw std::string Description of the operation type/parameters mismatch
+     */
     void expect_remove(pid_t pid, std::string path);
+
+    /**
+     * \brief Verifies that the first operation in the queue is a mkdir operation with the specified parameters
+     * \param pid Process id (or -1 for any process)
+     * \param path Directory path
+     * \throw std::string Description of the operation type/parameters mismatch
+     */
     void expect_mkdir(pid_t pid, std::string path);
+
+    /**
+     * \brief Verifies that the operation que is empty
+     * \throw std::string Description of the operation type/parameters mismatch
+     */
     void expect_empty_list();
 private:
+    /// A boolean flag, that specifies whether the operations should be put into the queue
     bool m_active = false;
+
+    /// The operation queue
     std::deque<OpRecord> m_records;
 };
 
